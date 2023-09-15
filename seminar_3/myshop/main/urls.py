f3dea469e4b7ddf9b3faafecf4b822dfc434372e
banderas91 <46642779+banderas91@users.noreply.github.com>
2023-09-15 21:12:55 +0300

    Delete seminar_3/myshop directory

--- seminar_3/myshop/main/urls.py
+++
@@ -1,9 +0,0 @@
-# main/urls.py
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index), 
-    path('clients/<int:client_id>/orders/', views.client_orders, name='client_orders'),
-]
